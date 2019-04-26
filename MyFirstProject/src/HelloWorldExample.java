a7f30a8e26ae50ba7b90736b895896c00ab60c42
Malmazoar <malmazoar@hotmail.com>
2019-04-27 09:14:15 +1200

    Comp

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the Users
+		// Output Hello World to the Computer
 		System.out.println("Hello World");
 	}
 
